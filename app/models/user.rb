259e9c1ed915682535d1a6b3e40a375e96a3fea6
谷村有寿麻 <t.azumax0502@gmail.com>
2021-12-20 22:54:33 +0900

    user情報のカラムバリデーションの設定

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :building_name, presence: true
+end
