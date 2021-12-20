5e8d8205ea5df92ea0d87a17780ae972b3c58012
谷村有寿麻 <t.azumax0502@gmail.com>
2021-12-20 22:55:03 +0900

    新規登録、ログインのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'api/auth/registrations'
+    }
+
+    namespace :auth do
+      resources :sessions, only: [:index]
+    end
+  end
+  # mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
