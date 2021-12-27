20a2caf373cbfb9f305785964aa5cb01f84af66f
谷村有寿麻 <t.azumax0502@gmail.com>
2021-12-27 21:20:47 +0900

    Userモデルのバリデーション変更

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_12_20_110605) do
     t.string "phone_number", null: false
     t.string "post_code", null: false
     t.string "address", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
