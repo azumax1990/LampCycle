cc82c1392c99a441c9f9b889a1757152812932c5
谷村有寿麻 <t.azumax0502@gmail.com>
2022-01-01 23:03:37 +0900

    bike情報入力ページのデザイン作成

---
+++ frontend/src/components/pages/BikePost.tsx
@@ -0,0 +1,86 @@
+import React, { VFC, memo } from 'react'
+import styled from 'styled-components'
+import { makers, weights } from '../../data'
+
+import { Header } from '../organisms/header/Header'
+import { SubmitButton } from '../atoms/SubmitButton' 
+
+const MainSection = styled.section`
+  width: 640px;
+  margin: 0 auto;
+  padding: 30px 0;
+`
+const PageTitle = styled.h1`
+  text-align: center;
+  margin: 0;
+`
+const MainSectionContainer = styled.div`
+  padding: 20px 40px;
+`
+const InputContainer = styled.div`
+  padding-bottom: 25px;
+`
+const InputTag = styled.input`
+  width: 100%;
+  font-size: 14px;
+  box-sizing:border-box;
+  padding: 15px;
+  margin-top: 10px;
+  border-radius: 4px;
+  border: 1px solid #9e9e9e;
+  outline: none;
+` 
+const LabelTag = styled.label`
+  font-size: 14px;
+  font-weight: bold;
+`
+
+const SelectTag = styled.select`
+  width: 100%;
+  padding: 15px;
+  margin-top: 10px;
+  outline: none;
+  font-size: 16px;
+`
+const OptionTag = styled.option`
+`
+
+export const BikePost: VFC = memo(() => {
+  return (
+    <>
+      <Header />
+      <MainSection>
+        <PageTitle>バイク登録</PageTitle>
+        <MainSectionContainer>
+          <InputContainer>
+            <LabelTag htmlFor='name'>名前</LabelTag>
+            <InputTag id='name'/>
+          </InputContainer>
+          <InputContainer>
+            <LabelTag htmlFor='maker'>メーカー</LabelTag>
+            <SelectTag id='maker'>
+              {makers.map((maker, index) => (
+                <OptionTag key={index}>{maker}</OptionTag>
+                )
+              )}
+            </SelectTag>
+          </InputContainer>
+          <InputContainer>
+            <LabelTag htmlFor='weight'>排気量</LabelTag>
+            <SelectTag id='weight'>
+              {weights.map((weight, index) => (
+                <OptionTag key={index} value={weight}>{weight}</OptionTag>
+                )
+              )}
+            </SelectTag>
+          </InputContainer>
+          <InputContainer>
+            <LabelTag htmlFor='image'>写真</LabelTag>
+            <InputTag id='image' type="file"/>
+          </InputContainer>
+          <SubmitButton value={"登録"}/>
+        </MainSectionContainer>
+      </MainSection>
+    </>
+  )
+})
